0807c24d60ec1cefd270e3a82c27de5c2ba17b4a
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-08-12 10:19:28 +0500

    ...

--- app/src/main/java/com/example/composebiometricauth/MainActivity.kt
+++ app/src/main/java/com/example/composebiometricauth/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         BiometricPromptManager(this)
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
